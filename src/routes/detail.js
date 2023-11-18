ed82971b09dd3953ad885c8382c212b1aeb1601c
Swikar Adhikari <swikara082@gmail.com>
2023-11-18 18:39:30 +1030

    feat: added mostWatchedChannel route

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-// import videoDetails from "../db/Schema/VideoDetails";
 import expressAsyncHandler from "express-async-handler";
 import userLogged from "../middleware/userlogged.js";
 import videoDetails from "../db/Schema/VideoDetails.js";
@@ -11,11 +10,46 @@ router.get(
   "/mostWatchedChannel",
   userLogged,
   expressAsyncHandler(async (req, res) => {
-    const user = await videoDetails
-      .findOne({ userEmail: res.req.user.email })
-      .exec();
+    try {
+      const user = await videoDetails
+        .findOne({ userEmail: res.req.user.email })
+        .exec();
 
-    console.log(user);
+      let watchedChannel = [];
+
+      user.data.map((user) => watchedChannel.push(user.channelTitle));
+
+      const sortedChannel = watchedChannel.sort();
+
+      let countWatchedChannels = {};
+      let count = 0;
+      let current = null;
+
+      for (let i = 0; i < sortedChannel.length; i++) {
+        if (sortedChannel[i] != current) {
+          if (count > 0) {
+            countWatchedChannels[current] = count;
+          }
+
+          current = sortedChannel[i];
+          count = 1;
+        } else {
+          count++;
+        }
+      }
+
+      if (countWatchedChannels) {
+        res
+          .status(200)
+          .json({ message: "Here is your data", data: countWatchedChannels });
+      } else {
+        res.status(401).json({ message: "not found" });
+      }
+    } catch (err) {
+      res
+        .status(200)
+        .json({ message: "Something went wrong while fetching the data", err });
+    }
   })
 );
 
