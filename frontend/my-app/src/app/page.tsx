b881c68933d23bc6a4c587b74f8edf1d5932b75a
Swikar Adhikari <swikara082@gmail.com>
2023-11-20 20:32:58 +1030

    feat: continuting tomorrow

--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
 
+const login = async () => {
+  try {
+    const response = await fetch("https://localhost:3000/api/auth/login");
+    const data = response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export default function Home() {
   return (
     <main className="flex  flex-col items-center justify-between p-24 gap-4">
-      <Button className="bg-zinc-200" variant="ghost">
+      <Button onClick={login} className="bg-zinc-200" variant="ghost">
         Sign In With Google <FcGoogle className="ml-2 w-5 h-5" />
       </Button>
 
